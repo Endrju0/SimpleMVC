e17114a392e3c5160cb7bac95d3b7282bec92248
Endrju0 <andrzej640@gmail.com>
2021-01-14 04:35:40 +0100

    Database update
    -> Added database class
    -> Added config
    -> Added a sample controller & view & model

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,10 @@
 <?php
+    // Database params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'rut');
+    define('DB_PASS', 'password');
+    define('DB_NAME', 'smvc');
+
     // App Root
     define('APP_ROOT',dirname(dirname(__FILE__)));
     // URL Root
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,13 +1,18 @@
 <?php
     class Pages extends Controller {
         public function __construct() {
-
+            $this->postModel = $this->model('Post');
         }
 
         // Index method is mandatory
         // call_user_func_array() expects parameter 1 to be a valid callback
         public function index() {
-            $this->view('pages/index', ['title' => 'Welcome']);
+            $posts = $this->postModel->getPosts();
+
+            $this->view('pages/index', [
+                'title' => 'Welcome',
+                'posts' => $posts
+            ]);
         }
 
         public function about() {
